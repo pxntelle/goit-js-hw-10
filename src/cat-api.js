a993cfd901d3d1f9441d905cc4fd071fadc4ef74
pxntelle <taniapantelle@gmail.com>
2023-06-04 10:33:08 +0200

    fetch settings fixed

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,24 +1,40 @@
 const BASE_URL = ' https://api.thecatapi.com/v1/';
 const BREEDS_ENDP = 'breeds';
 const ENDPOINT = 'images/search';
-const API_KEY = 'live_tgQbcnnirwR3modocPGn5ypKD5FvCB0XvReuZ5M58ssc4MlbuiAWZ3j22JoEimQb';
+const API_KEY =
+  'live_tgQbcnnirwR3modocPGn5ypKD5FvCB0XvReuZ5M58ssc4MlbuiAWZ3j22JoEimQb';
 
 const options = {
-    headers: {
-      'x-api-key': API_KEY,
-    },
-  };
+  headers: {
+    'x-api-key': API_KEY,
+  },
+};
 
 export function fetchBreeds() {
-    return fetch(`${BASE_URL}${BREEDS_ENDP}`, options).then(response =>
-      response.json()
-    );
-  }
-  
+  return fetch(`${BASE_URL}${BREEDS_ENDP}`, options)
+    .then(response => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        throw new Error(`Request failed with status code ${response.status}`);
+      }
+    })
+    .then(data => {
+      return {
+        ok: true,
+        data: data,
+      };
+    })
+    .catch(error => {
+      return {
+        ok: false,
+        error: error.message,
+      };
+    });
+}
 
-  export function fetchCatByBreed(breedId) {
-    return fetch(
-      `${BASE_URL}${ENDPOINT}?breed_ids=${breedId}`,
-      options
-    ).then(response => response.json());
-  }
+export function fetchCatByBreed(breedId) {
+  return fetch(`${BASE_URL}${ENDPOINT}?breed_ids=${breedId}`, options).then(
+    response => response.json()
+  );
+}
